f1ee38abe221f4352e6a7adff26e2b09dc2e6f95
Theekshana Jayalath <it23841918@my.sliit.lk>
2026-02-24 11:35:56 +0530

    feat: update package.json and server.js to include user and application routes, and add body-parser middleware

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 import connectDB from "./src/config/mongodb.js";
 import donationRoute from "./src/routes/DonationFormRoute.js";
+import authRoutes from "./src/routes/authRoutes.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import applicationRoutes from "./src/routes/applicationRoutes.js";
+
 
 
 dotenv.config();
@@ -10,6 +15,11 @@ const app = express();
 
 app.use(express.json());
 
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/applications", applicationRoutes);
 app.use("/donationForms", donationRoute);
 
 // connect database
@@ -25,6 +35,3 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("API Working 🚀");
-});
