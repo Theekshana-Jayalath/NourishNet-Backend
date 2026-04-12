d0a8840a97c8053cc80acddc85a6e68d202903e5
Theekshana Jayalath <165557105+Theekshana-Jayalath@users.noreply.github.com>
2026-04-12 11:46:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import express from "express";
 import cors from "cors";
 import requestRoutes from "./routes/request/request.routes.js";
+import donationRoute from "./routes/DonationFormRoute.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import applicationRoutes from "./routes/applicationRoutes.js";
+import deliveryRoutes from "./routes/delivery.routes.js";
+import ngoManagerRoutes from "./routes/ngoManagerRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
 
 const app = express();
 
@@ -19,7 +26,14 @@ app.use(
 // JSON parser
 app.use(express.json());
 
-// Your request routes
+// Your routes
 app.use("/api/requests", requestRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/applications", applicationRoutes);
+app.use("/api/donationForms", donationRoute);
+app.use("/api/deliveries", deliveryRoutes);
+app.use("/api/ngo-manager", ngoManagerRoutes);
+app.use("/api/admin", adminRoutes);
 
 export default app;
